95c269f2d6f6fc56ea297d32f41247a7e9677aa1
JoshuaHolloway <jhollow6@asu.edu>
2021-04-23 18:39:21 -0500

    component event via createEventDispatcher()

---
+++ 540_component-events/src/App.svelte
@@ -0,0 +1,21 @@
+<!-- https://youtu.be/9Bk7XFRMzgI -->
+<script>
+  import Button from './Button.svelte';
+
+  const f = () => {
+    console.log('f() invoked [Parent]');
+  };
+</script>
+
+<main>
+  <Button on:click={f}></Button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
