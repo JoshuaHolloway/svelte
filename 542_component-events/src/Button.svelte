ea4a541141363d4ad722f062b84e30b1283acbcc
JoshuaHolloway <jhollow6@asu.edu>
2021-04-23 18:35:19 -0500

    dispatch with effectively upwards dataflow

---
+++ 542_component-events/src/Button.svelte
@@ -0,0 +1,12 @@
+<script>
+  import {createEventDispatcher} from 'svelte';
+  const dispatch = createEventDispatcher();
+</script>
+
+<button on:click={() => dispatch('click_1', 1)}>
+  Button 1
+</button>
+
+<button on:click={() => dispatch('click_2', 2)}>
+  Button 2
+</button>
