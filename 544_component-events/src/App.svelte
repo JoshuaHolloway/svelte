50f4d480bd6a5c0f84daf0e006c2127441e69d4b
JoshuaHolloway <jhollow6@asu.edu>
2021-04-23 19:00:35 -0500

    add line item to end of cart

---
+++ 544_component-events/src/App.svelte
@@ -0,0 +1,37 @@
+<!-- https://youtu.be/9Bk7XFRMzgI -->
+<script>
+  import Button from './Button.svelte';
+  import LineItem from './LineItem.svelte';
+
+  let arr = ['zero', 'one', 'two', 'three'];
+
+  const f_remove = (e) => {
+    arr = arr.filter((elem, idx) => {
+      return idx !== e.detail
+    });
+  };
+
+  const f_add = () => {
+    arr = [...arr, String(arr.length)];
+  };
+
+
+</script>
+
+<main>
+  <Button on:add={f_add}></Button>
+
+  {#each arr as elem, idx}
+     <LineItem on:remove={f_remove} id={idx} name={elem}/>
+     <hr>
+  {/each}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
