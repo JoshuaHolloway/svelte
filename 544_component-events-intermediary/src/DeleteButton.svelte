bc43e7a8c4f9254333ffe8dd8289a927093ed0f0
JoshuaHolloway <jhollow6@asu.edu>
2021-04-23 20:54:01 -0500

    using an intermediary component

---
+++ 544_component-events-intermediary/src/DeleteButton.svelte
@@ -0,0 +1,15 @@
+<script>
+  import {createEventDispatcher} from 'svelte';
+  const dispatch = createEventDispatcher();
+  export let id;
+</script>
+
+<div on:click={() => dispatch('remove', id)}>
+  Delete Button
+</div>
+
+<style>
+  div {
+    background: darkorange;
+  }
+</style>
---
+++ 544_component-events-intermediary/src/LineItem.svelte
@@ -0,0 +1,19 @@
+<script>
+  import DeleteButton from './DeleteButton.svelte';
+
+  export let name;
+  export let id;
+  export let f;
+</script>
+
+<div>
+  <span>{id}: {name}</span>
+  <DeleteButton on:remove={f} id={id}/>
+</div>
+
+<style>
+  div {
+    background: deepskyblue;
+    padding: 10px;
+  }
+</style>
