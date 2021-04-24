76eeec4a8d44e3e83746cb0e5ad806d0a695e017
JoshuaHolloway <jhollow6@asu.edu>
2021-04-23 20:53:25 -0500

    using an intermediary component

---
+++ 540_component-events-intermediary/src/App.svelte
@@ -0,0 +1,21 @@
+<!-- https://youtu.be/9Bk7XFRMzgI -->
+<script>
+  import Component from './Component.svelte';
+
+  const f = () => {
+    console.log('f() invoked [Parent]');
+  };
+</script>
+
+<main>
+  <Component f={f} />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+</style>
---
+++ 540_component-events-intermediary/src/Component.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Button from './Button.svelte';
+
+  export let f;
+</script>
+
+<Button on:click={f}></Button>
